c6445012f6a8b01abf4d200ff5b781e1b4fdb16e
EC2 Default User <ec2-user@ip-172-31-13-130.us-east-2.compute.internal>
2018-05-09 01:59:00 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+   devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+  
+  
   resources :users, only: [:show]
   
   get 'users/show'
@@ -10,8 +14,8 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  
-  
+ 
+ 
   
   
   
